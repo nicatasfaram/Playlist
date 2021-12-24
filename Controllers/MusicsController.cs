5981988ae64dce9e1e16d2ec2b2e7d2faec94dc5
Nicat Asfaram <nicat.asfaram@gmail.com>
2021-12-24 23:44:14 +0300

    Add Playlist and Music Controller
    update ReadMe

---
+++ Controllers/MusicsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Playlist.Data;
+using Playlist.Models;
+
+namespace Playlist.Controllers
+{
+    public class MusicsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MusicsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Musics
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.musics.ToListAsync());
+        }
+
+        // GET: Musics/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var music = await _context.musics
+                .FirstOrDefaultAsync(m => m.MusicId == id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+
+            return View(music);
+        }
+
+        // GET: Musics/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Musics/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MusicId,Name,Album,Artist,RealiseData,Link")] Music music)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(music);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(music);
+        }
+
+        // GET: Musics/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var music = await _context.musics.FindAsync(id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+            return View(music);
+        }
+
+        // POST: Musics/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MusicId,Name,Album,Artist,RealiseData,Link")] Music music)
+        {
+            if (id != music.MusicId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(music);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MusicExists(music.MusicId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(music);
+        }
+
+        // GET: Musics/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var music = await _context.musics
+                .FirstOrDefaultAsync(m => m.MusicId == id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+
+            return View(music);
+        }
+
+        // POST: Musics/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var music = await _context.musics.FindAsync(id);
+            _context.musics.Remove(music);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MusicExists(int id)
+        {
+            return _context.musics.Any(e => e.MusicId == id);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Playlist
+﻿# Playlist
  Web-Programlama Proje
+
+ Proje amacı kullanıcıların sitede kendilerine ait müzik çalma liste oluşturup herkesle bu listeyi paylaşa bilmek. Her kullanıcı istenilen kadar çalma listesi (Playlist) oluşturup ve içeriğine isteniler kadar müzik ekleye bilir. 
+Projede User, Playlist, Music modelleri yer buluyor.
+
+User model property: string Name, string LastName, ICollection Playlist
+Playlist model property: string Name, int UserID, User user
+Music model property: int MusicID, string Album, string Name, string Artist, string RealiseDate, string Link
+
+Projede Authorization Authentication kavramları İdentityUser sınıfı yardımcılığıyla gerçekleştirilmiş.
+Login işlemi gerçekleştirildiğinde kullanıcı üst Nav-bar menüsünde Playlists ve Musics menülerine erişi biliyor.
+
+Projede UserRole ayrımı olarak admin kullanıcısı bulunuyor.
